b4abb4def41363c9eb98ba630bba29a2048e4eb0
kingkap4 <146883021+kingkap4@users.noreply.github.com>
2023-10-03 12:54:03 -0700

    Add files via upload

---
+++ hello-world.py
@@ -0,0 +1,17 @@
+#this program will write 
+#Hello World
+
+
+#this function tells the computer to print Hello World
+def PrintHelloWorld():
+	print("Hello World!")
+
+
+#this defines our main ()
+function for our program
+def main():
+	PrintHelloWorld()
+
+#when we run the program this executes first
+if __name__=="__main__":
+	main()
