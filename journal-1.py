41d1d00c202097d38febbbfde68026b075cf52b5
kingkap4 <146883021+kingkap4@users.noreply.github.com>
2023-10-07 15:26:46 -0700

    Add files via upload

---
+++ journal-1.py
@@ -0,0 +1,10 @@
+#this code will write my name, pronouns and one sentence about my favorite food.
+
+def PrintKapeeshwarSharma():
+	print("My name is Kapeeshwar Sharma. I also go by Kapeesh or Kap. I go by he him pronouns. My favorite food is a samosa.")
+
+def main():
+	PrintKapeeshwarSharma()
+
+if __name__ == '__main__':
+	main()
