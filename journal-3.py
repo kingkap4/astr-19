88e183332f279248cbd582330fd4e52dc6398e62
kingkap4 <146883021+kingkap4@users.noreply.github.com>
2023-10-17 22:07:38 -0700

    Add files via upload

---
+++ journal-3.py
@@ -0,0 +1,16 @@
+#this function will define a function and use an if statement
+
+def main():
+	f=(x**3+8)
+
+x = 3
+
+print (x**3+8)
+
+k = x**3+8
+
+if(k>27):
+	print("YAY")
+
+if __name__=="__main__":
+	main()
