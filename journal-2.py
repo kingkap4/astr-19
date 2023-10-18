bc0316a6a013d4005363d17b96b3ee432453da59
kingkap4 <146883021+kingkap4@users.noreply.github.com>
2023-10-17 17:59:36 -0700

    Add files via upload

---
+++ journal-2.py
@@ -0,0 +1,27 @@
+#this function will print the sum of two floating point numbers
+x = 20.0
+y = 30.0 
+
+print("x+y = ", x+y)
+z = x+y
+
+print(type(z))
+
+
+#this function will print the difference of two integers
+x = 74
+y = 19
+
+print("x-y = ", x-y)
+z = x-y
+print(type(z))
+
+
+#this function will print the product of a float and int
+x = 4.0
+y = 5
+
+print("x*y = ", x*y)
+z = x*y
+print(type(z))
+
